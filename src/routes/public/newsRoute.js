c5f52cbf5dfbbcb8a3913c0dfb4b720d3ddb28b1
imronshoimov <imronshoimov125@gmail.com>
2021-09-17 00:37:32 +0500

    render news

--- src/routes/public/newsRoute.js
+++ src/routes/public/newsRoute.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const model = require("../../model/news");
 
-router.get("/news", (req, res) => {
-  res.render("public/news");
+router.get("/news", async (req, res) => {
+  let news = await model.getNews();
+  res.render("public/news", { news });
 });
+
+router.get("/text", async (req, res) => {
+  let data = await model.getOneNews(req.query.id);
+  console.log(data)
+  res.render("public/text", { ...data });
+})
 
 module.exports = router;
