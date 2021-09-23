97a0bc3792f2598ae174736f9c49254cbc917286
Adham <muzaffarov.adham@gmail.com>
2021-09-23 15:54:30 +0500

    auth error solved

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -2,6 +2,6 @@ const jwt = require("jsonwebtoken");
 const { secretKey } = require("../config/keys");
 
 module.exports = {
-    sign: (playload) => jwt.sign(playload, secretKey),
+    sign: (playload) => jwt.sign(playload, secretKey, { expiresIn: "24h" }),
     verify: (token) => jwt.verify(token, secretKey)
 } 
--- src/middleware/redirect.js
+++ src/middleware/redirect.js
@@ -1,15 +1,31 @@
-const { verify } = require("../lib/jwt");
-const { fetch } = require("../lib/database")
+const jwt = require("../lib/jwt");
+const { fetch } = require("../lib/database");
 
 module.exports = async (req, res, next) => {
     const token = req.cookies.token;
-    const { username } = await fetch("select username from admin");
-    const payload = verify(token);
-    if(token && (username == payload.username)) {
-       return next();
+    if(!token) {
+        req.flash("error", "you are not logged in. Please login in to access. ")
+        res.redirect("/admin/login"); 
     } else {
-        req.flash("error", "You are not logged in. Please, login")
-        res.redirect("/admin/login");
+        try {
+            const payload = jwt.verify(token);  
+            const { username } = await fetch("select username from admin");
+            if(username === payload.username) {
+                return next()
+            } else {
+                req.flash("error", "The user belonging to this token does no longer exist.")
+                res.redirect("/admin/login");    
+            }
+        } catch (error) {
+          if(error.name === "JsonWebTokenError"){
+            req.flash("error", "Invalid token. Please, log in again!")
+            res.redirect("/admin/login");  
+          } else if(error.name === "JsonExpiredError") {
+            req.flash("error", "Your token has expired. Please, log in again!")
+            res.redirect("/admin/login");    
+          }
+        }
     }
+
 }
 
